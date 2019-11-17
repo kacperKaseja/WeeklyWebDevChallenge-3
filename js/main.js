fa3b0a3d2017f805d1bebc99fcc1a7354f13457e
Kacper Kaseja <yamakaszi10@o2.pl>
2019-11-17 13:38:02 +0100

    Ending styling and add html

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ const activeHamburger = () => {
 }
 
 hamburger.addEventListener('click', activeHamburger);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        menu.classList.remove('navigation__list--active');
+        hamburger.classList.remove('hamburger--active');
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            block: 'start',
+            behavior: 'smooth',
+        });
+    });
+});
